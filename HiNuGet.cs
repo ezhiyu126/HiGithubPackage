e49b01eafb0b9aa788eac1f51f487aedd6c6db2a
ezhiyu126 <ezhiyu@126.com>
2020-10-23 23:09:33 +0800

    第一个nuget 包

---
+++ HiNuGet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HiGithubPackage
+{
+    public class HiNuGet
+    {
+        /// <summary>
+        /// 设置信息
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Hi(string message)
+        {
+            Console.WriteLine(string.IsNullOrWhiteSpace(message) ? "Hi GithubPackage ~" : message);
+
+        }
+    }
+}
